7420f3ddd1406c09276bcd99a69633ff316da7b9
Lavina-23 <lavinakohai3227@gmail.com>
2023-10-19 14:00:08 +0700

    1.2 Object create

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // 1. Object literal
+// (-) Tidak bisa untuk objek yang banyak
 // let mahasiswa = {
 //     nama: 'Lavina',
 //     energy: 10,
@@ -9,42 +10,54 @@
 // }
 
 // 2. Function declaration
-// function Mahasiswa(nama, energy) {
-//     let mahasiswa = {};
-//     mahasiswa.nama = nama;
-//     mahasiswa.energy = energy;
+// (-) Memakan banyak penyimpanan
 
-//     mahasiswa.makan = function(porsi) {
+const methodMahasiswa = {
+    makan : function(porsi) {
+        this.energy += porsi;
+        console.log(`Halo ${this.nama}, selamat makan !`);
+    },
+    main : function (jam) {
+        this.energy -= jam;
+        console.log(`Halo ${this.nama}, selamat main`);
+    },
+
+    tidur : function (jam) {
+        this.energy += jam * 2;
+        console.log(`Halo ${this.name}, selamat tidur !`)
+    }
+};
+
+function Mahasiswa(nama, energy) {
+    let mahasiswa = Object.create(methodMahasiswa);
+    mahasiswa.nama = nama;
+    mahasiswa.energy = energy;
+    // mahasiswa.makan = methodMahasiswa.makan;
+    // mahasiswa.main = methodMahasiswa.main;
+    // mahasiswa.tidur = methodMahasiswa.tidur;
+
+    return mahasiswa;
+}
+
+let lavina = Mahasiswa('Lavina', 10);
+let yaya = Mahasiswa('Yaya', 20);
+
+// 3. Constructor function
+// function Mahasiswa(nama, energy) {
+//     this.nama = nama;
+//     this.energy = energy;
+
+//     this.makan = function(porsi) {
 //         this.energy += porsi;
 //         console.log(`Halo ${this.nama}, selamat makan !`);
 //     }
 
-//     mahasiswa.main = function (jam) {
+//     this.main = function (jam) {
 //         this.energy -= jam;
 //         console.log(`Halo ${this.nama}, selamat main`);
 //     }
-//     return mahasiswa;
 // }
 
-// let lavina = Mahasiswa('Lavina', 10);
-// let yaya = Mahasiswa('Yaya', 20);
-
-// 3. Constructor function
-function Mahasiswa(nama, energy) {
-    this.nama = nama;
-    this.energy = energy;
-
-    this.makan = function(porsi) {
-        this.energy += porsi;
-        console.log(`Halo ${this.nama}, selamat makan !`);
-    }
-
-    this.main = function (jam) {
-        this.energy -= jam;
-        console.log(`Halo ${this.nama}, selamat main`);
-    }
-}
-
-let lavina = new Mahasiswa('Lavina', 10);
+// let lavina = new Mahasiswa('Lavina', 10);
 
 // 4. Object.create
