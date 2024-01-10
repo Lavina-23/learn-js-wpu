d48796ca4778792bf47c53c02d880322f6846a16
Lavina-23 <lavinakohai3227@gmail.com>
2024-01-10 11:01:09 +0700

    feat: prototype

--- script.js
+++ script.js
@@ -12,35 +12,32 @@
 // 2. Function declaration
 // (-) Memakan banyak penyimpanan
 
-const methodMahasiswa = {
-    makan : function(porsi) {
-        this.energy += porsi;
-        console.log(`Halo ${this.nama}, selamat makan !`);
-    },
-    main : function (jam) {
-        this.energy -= jam;
-        console.log(`Halo ${this.nama}, selamat main`);
-    },
+// const methodMahasiswa = {
+//     makan : function(porsi) {
+//         this.energy += porsi;
+//         console.log(`Halo ${this.nama}, selamat makan !`);
+//     },
+//     main : function (jam) {
+//         this.energy -= jam;
+//         console.log(`Halo ${this.nama}, selamat main`);
+//     },
 
-    tidur : function (jam) {
-        this.energy += jam * 2;
-        console.log(`Halo ${this.name}, selamat tidur !`)
-    }
-};
+//     tidur : function (jam) {
+//         this.energy += jam * 2;
+//         console.log(`Halo ${this.name}, selamat tidur !`)
+//     }
+// };
 
-function Mahasiswa(nama, energy) {
-    let mahasiswa = Object.create(methodMahasiswa);
-    mahasiswa.nama = nama;
-    mahasiswa.energy = energy;
-    // mahasiswa.makan = methodMahasiswa.makan;
-    // mahasiswa.main = methodMahasiswa.main;
-    // mahasiswa.tidur = methodMahasiswa.tidur;
+// function Mahasiswa(nama, energy) {
+//     let mahasiswa = Object.create(methodMahasiswa);
+//     mahasiswa.nama = nama;
+//     mahasiswa.energy = energy;
+//     // mahasiswa.makan = methodMahasiswa.makan;
+//     // mahasiswa.main = methodMahasiswa.main;
+//     // mahasiswa.tidur = methodMahasiswa.tidur;
 
-    return mahasiswa;
-}
-
-let lavina = Mahasiswa('Lavina', 10);
-let yaya = Mahasiswa('Yaya', 20);
+//     return mahasiswa;
+// }
 
 // 3. Constructor function
 // function Mahasiswa(nama, energy) {
@@ -60,4 +57,50 @@ let yaya = Mahasiswa('Yaya', 20);
 
 // let lavina = new Mahasiswa('Lavina', 10);
 
-// 4. Object.create
+// prototype
+// function Mahasiswa(nama, energy) {
+//   this.nama = nama;
+//   this.energy = energy;
+// }
+
+// Mahasiswa.prototype.makan = function (porsi) {
+//   this.energy += porsi;
+//   return `Halo ${this.nama}, selamat makan !`;
+// };
+
+// Mahasiswa.prototype.main = function (jam) {
+//   this.energy -= jam;
+//   return `Halo ${this.nama}, selamat bermain !`;
+// };
+
+// Mahasiswa.prototype.tidur = function (jam) {
+//   this.energy += jam;
+//   return `Halo ${this.nama}, selamat tidur !`;
+// };
+
+// let lavina = new Mahasiswa("Lavina", 10);
+
+// prototype class version
+class Mahasiswa {
+  constructor(nama, energy) {
+    this.nama = nama;
+    this.energy = energy;
+  }
+
+  makan(porsi) {
+    this.energy += porsi;
+    return `Halo ${this.nama}, selamat makan !`;
+  }
+
+  main(jam) {
+    this.energy -= jam;
+    return `Halo ${this.nama}, selamat bermain !`;
+  }
+
+  tidur(jam) {
+    this.energy += jam;
+    return `Halo ${this.nama}, selamat tidur !`;
+  }
+}
+
+let lavina = new Mahasiswa("Lavina", 10);
