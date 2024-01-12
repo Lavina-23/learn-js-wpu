874d88a40f722394731ba2a608d6f7e6a9de9671
Lavina-23 <lavinakohai3227@gmail.com>
2024-01-12 16:25:26 +0700

    feat: arrow function

--- js/script2.js
+++ js/script2.js
@@ -67,3 +67,51 @@ let add2 = (function () {
 console.log(add2());
 console.log(add2());
 console.log(add2());
+
+// ARROW FUNCTION
+// from this
+const tampilNama = function (nama) {
+  return `Halo, ${nama}`;
+};
+console.log(tampilNama("Lavina"));
+
+// to this
+const tampilNama2 = (nama) => {
+  return `Halo, ${nama}`;
+};
+console.log(tampilNama2("Lavina"));
+
+// multiple params
+const tampilNama3 = (nama, waktu) => {
+  return `Selamat ${waktu}, ${nama}`;
+};
+console.log(tampilNama3("Lavina", "Malam"));
+
+// implicit return
+const tampilNama4 = (nama) => `Halo, ${nama}`;
+console.log(tampilNama4("Lavina"));
+
+// w/o param
+const tampilNama5 = () => {
+  return "Hello World";
+};
+console.log(tampilNama5());
+
+let mahasiswa = ["Lavina", "Aden", "Anais"];
+// w/o arrow function
+let jumlahHuruf = mahasiswa.map(function (nama) {
+  return nama.length;
+});
+console.log(jumlahHuruf);
+
+// w arrow function
+let jumlahHuruf1 = mahasiswa.map((nama) => nama.length);
+console.log(jumlahHuruf1);
+
+// return objects
+let jumlahHuruf2 = mahasiswa.map((nama) => ({
+  // nama, -> works
+  nama: nama,
+  jmlHuruf: nama.length,
+}));
+console.table(jumlahHuruf2);
