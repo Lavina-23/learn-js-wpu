67313bd5e808c0904d1bbc8ee56926ec969c30d2
Lavina-23 <lavinakohai3227@gmail.com>
2024-02-18 19:33:50 +0700

    feat: spread operator

--- js/script5.js
+++ js/script5.js
@@ -65,3 +65,39 @@ for (const p in prod) {
   //   console.log(p); // output: properties
   console.log(prod[p]); // output: propertie values
 }
+
+// Spread Operator
+// turns an iterable(like an array) into a single element
+const mhs1 = ["Lavina", "Maudy", "Ayunda"];
+console.log(...mhs1); // output: Lavina Maudy Ayunda
+console.log(...mhs1[0]); // output: L a v i n a
+
+// can be used to combine two array
+const mhs2 = ["Lavina", "Maudy", "Ayunda"];
+const dosen = ["Vivin", "Farida", "Baihaqi"];
+const combi = [...mhs2, "Mayla", ...dosen];
+console.log(combi); // array
+
+// copying arrays
+// const mhs3 = mhs2; // -> reference
+const mhs3 = [...mhs2];
+console.log(mhs3);
+
+// insert list into an array
+const liMhs = document.querySelectorAll("li");
+// use for loop
+const mhs4 = [];
+for (let i = 0; i < liMhs.length; i++) {
+  mhs4.push(liMhs[i].textContent);
+}
+console.log(mhs4);
+
+// use .map()
+// const mhs5 = liMhs.map((m) => m.textContent); // -> error because it's nodelist not an array
+const mhs5 = [...liMhs].map((m) => m.textContent); // -> use spread operator
+console.log(mhs5);
+
+const judul = document.querySelector(".judul");
+const huruf = [...judul.textContent].map((h) => `<span>${h}</span>`).join("");
+console.log(huruf);
+judul.innerHTML = huruf;
